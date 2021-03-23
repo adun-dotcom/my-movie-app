0d33263a3a4cebaaa77e6e412f1865ce9f8575ff
Adunola Johnson Odetola <aduntobi7@gmail.com>
2021-03-23 15:28:15 +0100

    modify and add file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// DOM ELEMENTS
 const apiTrending =
   'https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2021-01-10&primary_release_date.lte=2021-02-22&api_key=62bbe343222b3551f9b15d712b4d6b68&page=1'
 const apiWatchLater =
@@ -13,81 +14,64 @@ const SEARCH_URL =
 const IMAGE_URL = 'https://image.tmdb.org/t/p/w1280'
 const form = document.querySelector('#form')
 const modalContainer = document.querySelector('.modal-container')
+const modalOverlay = document.querySelector('.modal-overlay')
 const modalImage = document.querySelector('.modal-image')
 const modalOverview = document.querySelector('.modal-overview')
+const overView = document.querySelector('.overview')
+const gallerydiv = document.querySelector('.gallery-div')
 
-async function getMovies(url) {
+// FETCH MOVIE API
+const getMovies = async (url) => {
   const res = await fetch(url)
   const data = await res.json()
-  console.log(data.results)
   return data.results
 }
 
+// GET MOVIE Fectch
+
 getMovies()
 async function displayMovies1(api, selector) {
-  let movieData = await getMovies(api)
+  try {
+    let movieData = await getMovies(api)
 
-  document.querySelector(selector).innerHTML = ''
-  movieData.forEach((movie) => {
-    const { poster_path } = movie
-    const movieDiv = document.createElement('div')
-    movieDiv.classList.add('movie-div')
-    movieDiv.innerHTML = `
-            <img class='movie-img'  src="${IMAGE_URL + poster_path}" alt="">
-            <div class="scaled-icons ">
-                <div>
-                  <i class="fas fa-play"></i>
-                  <i class="fas fa-plus-circle"></i>
-                  <i class="far fa-thumbs-up"></i>
-                  <i class="far fa-thumbs-down"></i>
-                </div>
-                <div><i class="fas fa-chevron-down"></i></div>
-              </div>
+    document.querySelector(selector).innerHTML = ''
+    movieData.forEach((movie, i) => {
+      key = i
+      const { poster_path, id } = movie
+      const slider = document.createElement('div')
+      slider.classList.add('movie-div')
+      slider.innerHTML = `
+               <img class="img-${i} main-img" src="${IMAGE_URL + poster_path}" alt="">
+      
+      <div class="overview">
+        <div class="icon-text">          
+        Watch Trailer <i class="fas fa-play play" data-id="${id}"></i>
+      </div>
+      <img class="movie-img"  src='${IMAGE_URL + poster_path}' alt="">
+      </div>
+                    
+     
         `
-    document.querySelector(selector).appendChild(movieDiv)
-   
-    
-// modal reveal
-    // movieDiv.addEventListener('click', () => {
-    //   modalContainer.classList.remove('hidden')
-    //   modalImage.src = IMAGE_URL + poster_path
-    //  modalOverview.innerHTML = `<p>${movie.overview}</p>`
-   
-    // })
+      document.querySelector(selector).append(slider)
+    })
 
-
-    // movieDiv.addEventListener('mouseover', ()=>{
-    //   movieDiv.innerHTML = `
-    //   <div class='overlay-scale hidden >
-    //    <img class="scaled-image" src="${IMAGE_URL + poster_path}">
-    //             <div class="scaled-content">
-    //               <div class="scaled-icons">
-    //               <div>
-    //               <i class="fas fa-play"></i>
-    //               <i class="fas fa-plus-circle"></i>
-    //               <i class="far fa-thumbs-up"></i>
-    //               <i class="far fa-thumbs-down"></i>
-    //               </div>
-    //               <div><i class="fas fa-chevron-down"></i></div>
-    //             </div>
-    //             </div>
-    //             </div>
-    //   `
-    //   movieDiv.classList.add('scaled-div')
-    
-    // })
-    // modal hide
-    // modalContainer.addEventListener('click', ()=>{
-    //   modalContainer.classList.add('hidden')
-    // })
-  })
+    $(document).ready(function () {
+      $('.gallery-div').slick({
+        slidesToShow: 10,
+        slidesToScroll: 5,
+      })
+    })
+  } catch (e) {
+    console.log({ e })
+  }
 }
-
 displayMovies1(apiTrending, '.galleries1')
 displayMovies1(apiWatchLater, '.galleries2')
 displayMovies1(apiComedy, '.galleries3')
 displayMovies1(apiTopLists, '.galleries4')
 displayMovies1(apiFavorite, '.galleries5')
+
+// slide show
 
 form.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -98,12 +82,48 @@ form.addEventListener('submit', (e) => {
   } else window.location.reload()
 })
 
-// modal
+// GET MOVIE TRAILERS
+const getMovieVideos = async (id) => {
+  const res = await fetch(
+    `https://api.themoviedb.org/3/movie/${id}/videos?api_key=62bbe343222b3551f9b15d712b4d6b68`
+  )
+  const data = await res.json()
+  return data.results
+}
 
-// open modal eventlistener
+document.onclick = async function (event) {
+  try {
+    const target = event.target
+    if (target.classList.contains('play')) {
+      const movieID = target.dataset.id
+      modalContainer.classList.remove('hidden')
+      modalImage.innerHTML = `
+        <p style="color:white;">
+        loading...
+        </p>
+        `
+      const data = await getMovieVideos(movieID)
 
+      if (data.length) {
+        console.log(data)
+        const { key } = data[0]
+        modalImage.innerHTML = `
+        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/${key}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        <p>
+        `
+      } else {
+        modalContainer.classList.add('hidden')
+      }
+    }
+  } catch (e) {
+    modalContainer.classList.add('hidden')
+    console.log({ e })
+  }
+}
 
-//close modal eventlistener
-// closeModal.addEventListener('click', ()=>{
-//     modalContainer.classList.add('hidden')
-// })
+modalOverlay.addEventListener('click', () => {
+  if (!modalContainer.classList.contains('hidden')) {
+    modalContainer.classList.add('hidden')
+    modalImage.innerHTML = ''
+  }
+})
